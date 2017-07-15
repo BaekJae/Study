d56e2d590ec7d1749dfbd388fd009f3c6f9948e8
BaekJae <kis89576@naver.com>
2017-07-15 17:19:28 +0900

    Add files via upload

---
+++ Arithmetic7.java
@@ -0,0 +1,16 @@
+
+public class Arithmetic7 {
+
+	public static void main(String[] args) {
+		 
+				boolean A = (3 > 10) & (10 > 3);
+				boolean B = (3 > 10) && (10 > 3);
+				boolean C = (10 > 3) | (3 > 10);
+				boolean D = (10 > 3) || (3 > 10);
+				System.out.println("A=" +A+ " B=" +B+ " C=" +C+ " D=" +D);
+			
+		}
+
+	}
+
+
