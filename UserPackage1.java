b93df9a428d6c4caac6ff7641c9604eca753c81a
BaekJae <kis89576@naver.com>
2017-08-15 02:04:45 +0900

    Add files via upload
    package

---
+++ UserPackage1.java
@@ -0,0 +1,6 @@
+package com.magic;
+	public class UserPackage1{
+		public void printf(String str){
+			System.out.println(str);
+		}
+	}
---
+++ UserPackage2.java
@@ -0,0 +1,7 @@
+import com.magic.UserPackage1;
+public class UserPackage2{
+	public static void main(String[] args){
+		UserPackage1 obj = new UserPackage1();
+		obj.printf("패키지 사용 예");
+	}
+}
