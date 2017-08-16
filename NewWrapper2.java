1a6c9a5f28519d0ca9a95b3c6955b9c17f8d7290
BaekJae <kis89576@naver.com>
2017-08-17 01:28:32 +0900

    Add files via upload
    must set program arguments to run the program.

---
+++ NewWrapper2.java
@@ -0,0 +1,21 @@
+public class NewWrapper2{
+	public static void main(String[] args){
+		
+			if(args.length != 1){
+			System.out.println("사용법: java Wrapper2 문자열 ....");
+			System.exit(0);
+			}
+			char obj[]=new char[args[0].length()];
+			args[0].getChars(0,args[0].length(),obj,0);
+			for(int i=0;i<obj.length;i++){
+				System.out.println("입력된 문자 [" +obj[i]+"] :");
+				if(Character.isDefined(obj[i]))
+					System.out.println("유니코드입니다.");
+				if(Character.isDigit(obj[i]))
+					System.out.println("숫자입니다.");
+				if(Character.isLetter(obj[i]))
+					System.out.println("문자입니다.");
+				System.out.println("==================");
+			}
+	}	
+}
