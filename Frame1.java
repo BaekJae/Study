cf903298cb7cb1aa1c2bddede4210584f7881d6d
BaekJae <kis89576@naver.com>
2017-08-21 18:10:37 +0900

    Add files via upload

---
+++ Frame1.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+public class Frame1 {
+	public static void main(String[] args) {
+		Frame f = new Frame("윈도우 예제1");
+		f.setSize(200,200);
+		f.setVisible(true);
+	}
+}
---
+++ Frame2.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+public class Frame2 {
+	public static void main(String[] args) {
+		Frame f= new Frame();
+		f.setTitle("윈도우 예제2");
+		f.setBackground(Color.blue);
+		f.setLocation(200,200);
+		f.setSize(200, 200);
+		f.setVisible(true);
+	}
+}
